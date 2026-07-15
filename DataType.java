b53d465bb34586f5447a6742041bb6d2aa189cc7
jahnaviarasavelli07 <jahnaviarasavelli2007@gmail.com>
2026-07-15 21:04:01 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class DataType
+{
+  public static void main(String args[])
+   {
+    byte b=10;
+    short s=20;
+    int a=3;
+    float f=25.5f;
+    double d=333.5674;
+    long l=4500000000L;
+    char ch='a';
+    boolean i= true;
+    System.out.println(b);
+    System.out.println(s);
+    System.out.println(a);
+    System.out.println(f);
+    System.out.println(d);
+    System.out.println(l);
+    System.out.println(ch);
+    System.out.println(i);
+   }
+}
