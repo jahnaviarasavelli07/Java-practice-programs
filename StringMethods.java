ca483ae9c9fdb5cb20ff322328ddebdf49e87a9b
jahnaviarasavelli07 <jahnaviarasavelli2007@gmail.com>
2026-07-23 21:47:38 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,13 @@
+class StringMethods
+{
+	public static void main(String args[])
+	{
+		String str="Sri Krishna";
+		System.out.println("Upper"+str.toUpperCase());
+		System.out.println("Lower"+str.toLowerCase());
+		System.out.println("Lentgh"+str.length());
+		System.out.println("Concatenate"+str.concat("! Hello"));
+		System.out.println("Position"+str.charAt('a'));
+		System.out.println("Index"+str.indexOf(' '));
+	}
+}
