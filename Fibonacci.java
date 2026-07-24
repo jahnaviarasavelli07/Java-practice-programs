956eb723f38a4fa4365e00e6215cf42d84f3d795
jahnaviarasavelli07 <jahnaviarasavelli2007@gmail.com>
2026-07-24 21:44:36 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Fibo
+{
+   public static void main(String args[])
+   {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number:");
+	int n = sc.nextInt();
+	int t1=0,t2=1;
+	System.out.println(" "+t1+" "+t2);
+	for(int i=2;i<n;i++)
+	{
+	   int t3=t1+t2;
+	   System.out.println(" "+t3);
+	   t1=t2;
+	   t2=t3;
+	}
+   }
+}
