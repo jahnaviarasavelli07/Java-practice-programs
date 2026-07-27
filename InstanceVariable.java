2bb0adca6905f7d44b9a62dd7597ec6ef00fe7e6
jahnaviarasavelli07 <jahnaviarasavelli2007@gmail.com>
2026-07-27 21:24:34 +0530

    Add files via upload

---
+++ InstanceVariable.java
@@ -0,0 +1,9 @@
+class InstanceVariable
+{
+   int a=72;
+   public static void main(String args[])
+   {
+    	InstanceVariable obj = new InstanceVariable();
+   	System.out.println("Instance Variable is "+obj.a);
+   }
+}
